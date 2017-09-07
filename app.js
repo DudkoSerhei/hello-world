aa9ddbd25760d03fcce68e6f4c9d518184daba24
DudkoSerhei <serhei.dudko@gmail.com>
2017-09-07 18:16:04 +0300

    Fix app.js

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var favicon = require('serve-favicon');
 
 var app = express();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
-app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(express.static(__dirname + '/public'));
+// app.use(favicon(__dirname + './public/favicon.ico'));
 
-app.get('/', index.index);
+app.get('/', function (req, res){
+  res.send('hello world')
+});
 
 app.listen(3000, function () {
   console.log('Server is Up!');
