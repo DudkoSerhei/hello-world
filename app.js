530aedf5f139fa956b51b114738732c687c201a0
DudkoSerhei <serhei.dudko@gmail.com>
2017-09-07 16:17:32 +0300

    Add hello-world server

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var index = require('./routes/index.js');
+var favicon = require('serve-favicon');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.get('/', index.index);
+
+app.listen(3000, function () {
+  console.log('Server is Up!');
+});
