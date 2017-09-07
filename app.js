75439bc4c9b2410cffa5a8b42a27c3c0ae59ae9d
DudkoSerhei <serhei.dudko@gmail.com>
2017-09-07 18:27:29 +0300

    Fix app get routes

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
-app.get('/', function (req, res){
-  res.send('hello world')
-});
-
 var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
